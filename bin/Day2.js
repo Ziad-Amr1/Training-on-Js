4ad57752ac2f7ed9c122b86e2bcc9bb9a5e5cd3c
Ziad Amr <Zyadamr177@gmail.com>
2025-08-17 17:36:05 +0300

    تجاهل ملفات JS في bin/

---
+++ bin/Day2.js
@@ -0,0 +1,19 @@
+let degree = 85;
+
+if (degree % 2 === 0) {
+    console.log('Even Degree');
+} else {
+    console.log('Odd Degree');
+}
+
+if (degree === 100) {
+    console.log('You are a genius!');
+} else if (degree >= 90) {
+    console.log('Excellent 🎉');
+} else if (degree >= 70) {
+    console.log('Good 😊');
+} else if (degree >= 50) {
+    console.log('Pass 🙂');
+} else {
+    console.log('Fail ❌');
+}
---
+++ bin/script.js
@@ -0,0 +1,36 @@
+var Lvl = 5;
+console.log(Lvl);
+Lvl = 4;
+console.log(Lvl);
+
+let Lvls = 7;
+console.log(Lvls);
+Lvls = 6;
+console.log(Lvls);
+
+const DLvl = 8;
+console.log(DLvl);
+DLvl = 9;
+console.log(DLvl);
+
+let word = "Hello";
+console.log(word, typeof word, word.length);
+
+let num = 5;
+console.log(num, typeof num);
+
+let bool = true;
+console.log(bool, typeof bool);
+
+let nul 
+console.log(nul, typeof nul);
+
+let arr = [1, 2, 3];
+console.log(arr, typeof arr, arr.length, arr[0]);
+
+let obj = {
+  name: "Kayda",
+  age: 20,
+  gender: "Female",
+};
+console.log(obj, typeof obj, obj.name, obj.age, obj.gender);
