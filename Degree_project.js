6f83b1a56dfd3f7172faea8ac55154b88543e42f
Ziad Amr <Zyadamr177@gmail.com>
2025-08-17 17:19:38 +0300

    add no_need file to ignore

---
+++ Degree_project.js
@@ -0,0 +1,124 @@
+let allStudents = [
+    { id: 1, name: "Kayda", degree: 100 },
+    { id: 2, name: "Diya", degree: 85 },
+    { id: 3, name: "Ravi", degree: 73 },
+    { id: 4, name: "Rahul", degree: 60 },
+    { id: 5, name: "Ray", degree: 45 },
+    { id: 6, name: "Raj", degree: 36 },
+    { id: 7, name: "Kaushik", degree: 27 },
+];
+
+function getAllStudentsDegree() {
+
+    let sum = 0;
+
+    console.log('Degrees of all students:');
+
+    for (let person of allStudents) {
+        sum += person.degree;
+        
+    switch(true) {
+        case (person.degree === 100):
+            console.log(person.name + ' is a genius!');
+            break;
+        case (person.degree >= 85 && person.degree <= 99):
+            console.log(person.name + ' is an excellent student!');
+            break;
+        case (person.degree >= 70 && person.degree < 85):
+            console.log(person.name + ' is a very good student!');
+            break;
+        case (person.degree >= 60 && person.degree < 70):
+            console.log(person.name + ' is a good student!');
+            break;
+        case (person.degree >= 50 && person.degree < 60):
+            console.log(person.name + ' is a pass student!');
+            break;
+        case (person.degree < 50):
+            console.log(person.name + ' is a fail student!');
+            break;
+        default:
+            console.log(person.name + ' has an invalid degree!');
+    }
+    }
+    return { sum };
+}
+
+// function getAverageOfAllStudents() {
+//     let { sum } = getAllStudentsDegree();
+//     let avr = sum / allStudents.length;
+//     return { avr };
+// }
+
+// function printAllStudentsAverage() {
+//     let { avr } = getAverageOfAllStudents();
+//     avr = parseFloat(avr.toFixed(2));
+//     console.log('Total count of students = ' + allStudents.length);
+//     console.log('Average for all students = ' + avr);
+// }
+
+function getPassedStudents() {
+
+    let sum2 = 0;
+    let count = 0;
+
+    console.log('Degrees of students who passed:');
+    for (let person of allStudents) {
+        if (person.degree >= 50) {
+            sum2 += person.degree;
+            count++;
+            console.log(person.name + ' : ' + person.degree);
+        }
+
+    }
+    // return sum2 / count;
+    return { sum2, count };
+}
+
+// function getAverageOfPassedStudents() {
+//     let { sum2, count } = getPassedStudents();
+//     let avr2 = sum2 / count;
+//     return { avr2, count };
+// }
+
+// function printPassedStudentsAverage() {
+//     let { avr2, count } = getAverageOfPassedStudents();
+//     avr2 = parseFloat(avr2.toFixed(2));
+//     console.log('Total count of students who passed = ' + count);
+//     console.log('Average for students successed = ' + avr2);
+// }
+
+function Calc(){
+    let { sum } = getAllStudentsDegree();
+    let { sum2, count } = getPassedStudents();
+    let avr = sum / allStudents.length;
+    let avr2 = sum2 / count;
+    
+    avr = parseFloat(avr.toFixed(2));
+    avr2 = parseFloat(avr2.toFixed(2));
+    count = parseInt(count);
+    
+    return { avr, avr2, count };
+}
+
+// function getAverage() {
+//     let { avr, avr2, count } = Calc();
+
+//     avr = parseFloat(avr.toFixed(2));
+//     avr2 = parseFloat(avr2.toFixed(2));
+//     count = parseInt(count);
+
+//     return { avr, avr2, count };
+// }
+
+function printAllAverage() {
+    let { avr, avr2, count } = Calc();
+
+    console.log('Total count of students = ' + allStudents.length);
+    console.log('Average for all students = ' + avr);
+    console.log('Average for students who passed = ' + avr2);
+    console.log('Total count of students who passed = ' + count);
+};
+
+// printAllStudentsAverage();
+// printPassedStudentsAverage();
+printAllAverage();
